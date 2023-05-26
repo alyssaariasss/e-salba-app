2a459977eb456c15b601a78d3a5f5785b86cc3a7
Alyssa Arias <alyssaarias27@gmail.com>
2023-05-26 11:52:58 +0800

    added fragment layouts and bottom nav

--- app/src/main/java/com/example/esalba/Home.java
+++ app/src/main/java/com/example/esalba/Home.java
@@ -1,14 +1,46 @@
 package com.example.esalba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+import com.example.esalba.databinding.ActivityHomeBinding;
 
+public class Home extends AppCompatActivity {
+    ActivityHomeBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.summary:
+                    replaceFragment(new SummaryFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager =  getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
